2ff7698cba99feedbf0f0ff7db090e47aa9405fd
AaronPersaud2007 <92407873+AaronPersaud2007@users.noreply.github.com>
2022-02-17 17:54:53 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var car, gasoline
+
+function preload(){
+  gasoline = loadImage("gasoline.png")
+  car = loadImage("car.png")
+}
+function setup() {
+  
+}
+
+
+function draw() {
+  
+
+    drawSprites();
+  }
+    
+  
+
+
+
